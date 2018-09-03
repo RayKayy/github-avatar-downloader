7d1db38d0ee56bade3063f1fac39b1334a0dd1ae
Raymond Kwan <rayk199492@gmail.com>
2018-09-03 18:32:32 +0000

    Updated main codes and README, included secrets in gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,19 @@
 const request = require('request');
+const secrets = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+function getRepoContributors(repoOwner, repoName, callback) {
+  const options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'avatar-downloader',
+      Authorization: `token ${secrets.GITHUB_TOKEN}`,
+    },
+  };
+  request(options, (err, res, body) => {
+    callback(err, body);
+  });
 }
 
 getRepoContributors('jquery', 'jquery', (err, res) => {
