030c1d4fae789c34be9409c1be6b3dd45d08bbed
Raymond Kwan <rayk199492@gmail.com>
2018-09-03 18:11:19 +0000

    Added outlines for main code

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,12 @@
 const request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+getRepoContributors('jquery', 'jquery', (err, res) => {
+  console.log('Errors:', err);
+  console.log('Result:', res);
+});
